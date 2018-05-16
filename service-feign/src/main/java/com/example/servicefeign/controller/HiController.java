1ce74afbd0f8d68ac1ab184e678df627a18c8311
lixin <823523036@qq.com>
2018-05-16 09:41:15 +0800

    初始版本

---
+++ service-feign/src/main/java/com/example/servicefeign/controller/HiController.java
@@ -0,0 +1,18 @@
+package com.example.servicefeign.controller;
+
+import com.example.servicefeign.service.SchedualServiceHi;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+public class HiController {
+
+    @Autowired
+    SchedualServiceHi schedualServiceHi;
+
+    @RequestMapping(value = "/hi",method = RequestMethod.GET)
+    public String sayHi(@RequestParam String name){
+        return schedualServiceHi.sayHiFromClientOne(name);
+    }
+
+}
